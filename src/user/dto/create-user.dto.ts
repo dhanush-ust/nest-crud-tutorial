48840489f9b8e007080e8d7d0e97a8858684261f
dhanush-ust <287641@ust.com>
2024-10-16 15:14:00 +0530

    gtyugu

---
+++ src/user/dto/create-user.dto.ts
@@ -0,0 +1,21 @@
+import{
+        IsEmail,
+        IsNotEmpty,
+        IsString,
+
+}from 'class-validator';
+
+
+export class CreateUserDto {
+@IsNotEmpty()
+@IsString()
+fullName:string;
+
+@IsNotEmpty()
+@IsString()
+email:string;
+
+
+
+
+}
